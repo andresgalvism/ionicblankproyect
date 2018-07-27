a98b990504f060b89b66ba6ec25bc658ef048e82
Angela Tenjo <angelatenjoc@gmail.com>
2018-07-26 20:24:19 -0500

    proyecto 1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,54 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public alertCtrl: AlertController) {
 
   }
+
+  showAlert() {
+    let alert = this.alertCtrl.create({
+      title: '¡Alert!',
+      subTitle: 'access denied for user',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+  showPrompt() {
+    let prompt = this.alertCtrl.create({
+      title: 'Recover your email',
+      message: "enter your mail",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Email'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            console.log('Saved clicked');
+            this.showAlert1(data);
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  showAlert1(data) {
+    let alert = this.alertCtrl.create({
+      title: 'Send!',
+      subTitle: 'an email was sent to '+ data['title'],
+      buttons: ['OK']
+    });
+    alert.present();
+}
 
 }
